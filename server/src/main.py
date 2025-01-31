0e696b342c8ddd718ccfe15ef94d1227e0c6600c
Yash Ori <yash.ori498@gmail.com>
2025-01-31 21:17:55 +0000

    moved load_dotenv call

--- server/src/main.py
+++ server/src/main.py
@@ -1,8 +1,12 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from .routers.api_router import router as api_router
+
+load_dotenv("../.env")
+
 
 app: FastAPI = FastAPI()
 
